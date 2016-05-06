a1063006bb918dc6c94571a5513c354e59bc2db8
mjanicki <mjanicki@1V0DJ72>
2016-05-06 11:39:37 +0200

    gulp configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-﻿/// <binding BeforeBuild='copy-bootstrap' ProjectOpened='copy-bootstrap' />
+﻿/// <binding BeforeBuild='default' ProjectOpened='default' />
 /*
 This file in the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
@@ -6,7 +6,8 @@ Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 
 var gulp = require('gulp');
 var del = require('del');
-var runSequence = require('run-sequence');
+var watch = require('gulp-watch');
+var less = require('gulp-less');
 
 var destinationDistributionPath = 'Library/Resources';
 var destinationSourcePath = 'Library/ExternalSource';
@@ -14,34 +15,60 @@ var destinationScriptsPath = destinationDistributionPath + '/scripts';
 var destinationStylesPath = destinationDistributionPath + '/styles';
 var destinationContentPath = destinationDistributionPath + '/content';
 
+var contentPath = "Library/Content";
+var lessContentPath = contentPath + '/**/*.less';
+
 var sourcePath = 'bower_components';
 var bootstrapSourcePath = sourcePath + '/bootstrap';
 var jquerySourcePath = sourcePath + '/jquery';
+var reactSourcePath = sourcePath + '/react';
 
-gulp.task('default', () =>{
-    runSequence('cleanup', ['copy-bootstrap', 'copy-jquery']);
+gulp.task('watch-less', () =>{
+    return watch(lessContentPath, gulp.series('compile-less'));
 });
 
-gulp.task('cleanup', () => {
+gulp.task('compile-less', () => {
+    return gulp.src(lessContentPath).
+        pipe(less()).
+        pipe(gulp.dest(destinationStylesPath));
+});
+
+gulp.task('cleanup', function(){
     return del([destinationDistributionPath, destinationSourcePath]);
 });
 
-gulp.task('copy-bootstrap', () =>{
-    return runSequence(['copy-bootstrap-fonts', 'copy-bootstrap-less', 'copy-bootstrap-js']);
-});
-
 gulp.task('copy-bootstrap-fonts', () => {
-    return gulp.src(bootstrapSourcePath + '/dist/fonts/**/*.*').pipe(gulp.dest(destinationContentPath + '/bootstrap/fonts'));
+    return gulp.src(bootstrapSourcePath + '/dist/fonts/**/*.*').
+        pipe(gulp.dest(destinationContentPath + '/bootstrap/fonts'));
 });
 
 gulp.task('copy-bootstrap-less', () => {
-    return gulp.src(bootstrapSourcePath + '/less/**/*.*').pipe(gulp.dest(destinationSourcePath + '/bootstrap/less'));
+    return gulp.src(bootstrapSourcePath + '/less/**/*.*').
+        pipe(gulp.dest(destinationSourcePath + '/bootstrap/less'));
+});
+
+gulp.task('copy-bootstrap-css', () => {
+    return gulp.src(bootstrapSourcePath + '/dist/css/bootstrap.css').
+        pipe(gulp.dest(destinationStylesPath));
 });
 
 gulp.task('copy-bootstrap-js', () => {
-    return gulp.src(bootstrapSourcePath + '/dist/js/bootstrap.js').pipe(gulp.dest(destinationScriptsPath));
+    return gulp.src(bootstrapSourcePath + '/dist/js/bootstrap.js').
+        pipe(gulp.dest(destinationScriptsPath));
 });
 
 gulp.task('copy-jquery', () => {
-    return gulp.src(jquerySourcePath + '/dist/jquery.js').pipe(gulp.dest(destinationScriptsPath))
+    return gulp.src(jquerySourcePath + '/dist/jquery.js').
+        pipe(gulp.dest(destinationScriptsPath))
 });
+
+gulp.task('copy-react', () => {
+    return gulp.src([reactSourcePath + '/react.js', reactSourcePath + '/react-dom.js']).
+        pipe(gulp.dest(destinationScriptsPath))
+});
+
+gulp.task('copy-bootstrap', gulp.parallel('copy-bootstrap-fonts', 'copy-bootstrap-css', 'copy-bootstrap-less', 'copy-bootstrap-js'));
+
+gulp.task('start-dev', gulp.series('cleanup', gulp.parallel('copy-bootstrap', 'copy-jquery','copy-react'), gulp.parallel('compile-less')));//, 'watch-less'
+
+gulp.task('default', gulp.series('start-dev'));
